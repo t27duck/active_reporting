ad4dcc3c40e6d121e4d53702237b6bfdf980745f
Tony Drake <t27duck@gmail.com>
2017-01-18 17:29:47 -0500

    Tweaks to Report tests

--- test/active_reporting/report_test.rb
+++ test/active_reporting/report_test.rb
@@ -3,10 +3,14 @@ require 'test_helper'
 class ActiveReporting::ReportTest < Minitest::Test
   def setup
     @metric = ActiveReporting::Metric.new(:a_metric, fact_model: PostFactModel, dimensions: [:state])
+    @report = ActiveReporting::Report.new(@metric)
   end
 
   def test_run_returns_an_array
-    report = ActiveReporting::Report.new(@metric)
-    assert report.run.is_a?(Array)
+    assert @report.run.is_a?(Array), 'result is not an array'
+  end
+
+  def test_result_contains_the_metric_name
+    assert @report.run.all? { |r| r.key?(@metric.name.to_s) }, 'metric name not included'
   end
 end
