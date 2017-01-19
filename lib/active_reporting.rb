55c410f99567bdf0605bfcf926efb3c95459e8d7
Tony Drake <t27duck@gmail.com>
2017-01-18 19:49:50 -0500

    Moved errors out of their own file for now

--- lib/active_reporting.rb
+++ lib/active_reporting.rb
@@ -3,7 +3,6 @@ require 'active_reporting/active_record_adaptor'
 require 'active_reporting/configuration'
 require 'active_reporting/dimension'
 require 'active_reporting/dimension_filter'
-require 'active_reporting/error'
 require 'active_reporting/metric'
 require 'active_reporting/fact_model'
 require 'active_reporting/report'
@@ -17,3 +16,47 @@ rescue
 end
 
 ActiveRecord::Base.extend(ActiveReporting::ActiveRecordAdaptor)
+
+module ActiveReporting
+  class UnknownDimension < StandardError
+    def initialize(name, fact_model)
+      @name       = name
+      @fact_model = fact_model
+    end
+
+    def message
+      "Dimension '#{@name}' not found on fact model '#{@fact_model}'"
+    end
+    alias to_s message
+  end
+
+  class UnknownAggregate < StandardError
+    def initialize(name)
+      @name = name
+    end
+
+    def message
+      "Unknown aggregate '#{@name}'"
+    end
+    alias to_s message
+  end
+
+  class UnknownDimensionFilter < StandardError
+    def initialize(name, fact_model)
+      @name       = name
+      @fact_model = fact_model
+    end
+
+    def message
+      "Dimension filter '#{@name}' not found on fact model '#{@fact_model}'"
+    end
+    alias to_s message
+  end
+
+  class RansackNotAvailable
+    def message
+      'Ransack is not available. Please include it in your Gemfile.'
+    end
+    alias to_s message
+  end
+end
